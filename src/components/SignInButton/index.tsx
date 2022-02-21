c99ae4ac72fd67726384356d21512bdcc66c43a5
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-02-21 20:56:19 -0300

    Update index.tsx

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,29 @@
 import styles from "./styles.module.scss";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 import { FaGithub } from "react-icons/fa";
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const { data: session, status } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.SingInButton}>
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.SingInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Denner augusto
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.SingInButton}>
+    <button
+      type="button"
+      className={styles.SingInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sing in with Github
     </button>
