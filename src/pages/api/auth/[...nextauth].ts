6a69115b25d13bc86ee75bc1d2807d0c30cbdc5d
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-02-28 23:57:02 -0300

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna';
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,8 +10,35 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-     
-    }),
-   
+      authorization:{params: { scope: "user" }}
+    })
+    
   ],
+
+
+
+  
+  callbacks:{
+    async signIn({ user, account, profile,  credentials }) {
+
+      const {email} = user
+          
+    try {
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {data:{email}}
+        )
+      )
+      return true
+    } catch (error) {
+      return false
+    }
+      
+    },
+  },
+  secret:process.env.NEXTAUTH_URL
+  
+  
+   
 })
