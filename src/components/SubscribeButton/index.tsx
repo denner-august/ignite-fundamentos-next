c8f56629adec6136d31c0d9e80335b5200785c7a
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-01 21:12:24 -0300

    Update index.tsx

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,27 @@
 import styles from "./styles.module.scss";
+import { useSession, signIn } from "next-auth/react";
+import subscribe from "../../pages/api/subscribe";
 interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function headSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    // checkout session
+  }
+
   return (
-    <button type="button" className={styles.SubscribeButton}>
+    <button
+      type="button"
+      className={styles.SubscribeButton}
+      onClick={headSubscribe}
+    >
       subscribe now
     </button>
   );
