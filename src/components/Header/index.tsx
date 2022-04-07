d51abc53fa5943981ff960d362b61237b2cca4f4
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-04-06 21:05:35 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
 import { SingInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
-
+import { ActiveLink } from "../Activelink";
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>post</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>post</a>
+          </ActiveLink>
         </nav>
 
         <SingInButton />
