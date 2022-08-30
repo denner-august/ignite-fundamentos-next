ea79fcba152e7680aa4eeb23ceccb405a0ea3645
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-08-30 13:38:52 -0300

    removendo tipagem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
-import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { SessionProvider } from "next-auth/react";
 import "../styles/Global.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Header />
