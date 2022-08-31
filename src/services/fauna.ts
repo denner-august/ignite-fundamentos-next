f865789d975ddc3c297cc9ee91d9730354fa5c0c
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-08-31 13:33:28 -0300

    deploy teste

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,7 @@
 import { Client } from "faunadb";
 
+console.log(process.env.FAUNA_DB);
+
 export const fauna = new Client({
-    secret: process.env.FAUNA_DB
-})
+  secret: process.env.FAUNA_DB,
+});
